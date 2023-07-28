09626b12a02bde520859dbda3c6a9313677c37fd
devmisterio <arslan_semih@yahoo.com>
2023-07-28 10:43:50 +0300

    choose and read file

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Something...");
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+// __filename and __dirname are not available in ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// aim is to get a random file from the texts folder
+const filenames = fs.readdirSync(path.join(__dirname, "texts"));
+
+// get a random index from the filenames array (0 to length - 1)
+const randomFileIndex = Math.floor(Math.random() * filenames.length) + 1;
+
+// get the file path
+const filePath = path.join(__dirname, "texts", filenames[randomFileIndex]);
+
+// read the file with readStream
+const fileContent = fs.readFileSync(filePath, "utf-8");
+
+console.log(fileContent);
